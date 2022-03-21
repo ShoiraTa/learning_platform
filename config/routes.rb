a3219cb35a936d8e79315983d6ba370ea40d16f0
shoiraTa <shoira.shakirovna@gmail.com>
2022-03-21 17:16:21 +0400

    Add friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
+  get 'my_friends', to: 'users#my_friends'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
